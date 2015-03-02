6f9435709b4dacb7eeba02a46f08d9058e23df5a
Leslie Strauss <leslie.strauss@gmail.com>
2015-03-02 00:06:59 -0800

    Controller added for graph color and values.

---
+++ app/controllers/index.js
@@ -0,0 +1,119 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  pieValue1: 300,
+  pieValue2: 50,
+  pieValue3: 100,
+  pieData: function() {
+    return [{
+      value: parseInt(this.get('pieValue1')),
+      color: "#F7464A",
+      highlight: "#FF5A5E",
+      label: "Red"
+    }, {
+      value: parseInt(this.get('pieValue2')),
+      color: "#46BFBD",
+      highlight: "#5AD3D1",
+      label: "Green"
+    }, {
+      value: parseInt(this.get('pieValue3')),
+      color: "#FDB45C",
+      highlight: "#FFC870",
+      label: "Yellow"
+    }];
+  }.property('pieValue1', 'pieValue2', 'pieValue3'),
+  lineValue1: 65,
+  lineValue2: 59,
+  lineData: function() {
+    return {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [{
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [parseInt(this.get('lineValue1')), parseInt(this.get('lineValue2')), 80, 81, 56, 55, 40]
+      }, {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }]
+    };
+  }.property('lineValue1', 'lineValue2'),
+  barData: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+      label: "My First dataset",
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }, {
+      label: "My Second dataset",
+      fillColor: "rgba(151,187,205,0.5)",
+      strokeColor: "rgba(151,187,205,0.8)",
+      highlightFill: "rgba(151,187,205,0.75)",
+      highlightStroke: "rgba(151,187,205,1)",
+      data: [28, 48, 40, 19, 86, 27, 90]
+    }]
+  },
+  radarData: {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [{
+      label: "My First dataset",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: [65, 59, 90, 81, 56, 55, 40]
+    }, {
+      label: "My Second dataset",
+      fillColor: "rgba(151,187,205,0.2)",
+      strokeColor: "rgba(151,187,205,1)",
+      pointColor: "rgba(151,187,205,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(151,187,205,1)",
+      data: [28, 48, 40, 19, 96, 27, 100]
+    }]
+  },
+  polarAreaData: [{
+      value: 300,
+      color: "#F7464A",
+      highlight: "#FF5A5E",
+      label: "Red"
+    }, {
+      value: 50,
+      color: "#46BFBD",
+      highlight: "#5AD3D1",
+      label: "Green"
+    }, {
+      value: 100,
+      color: "#FDB45C",
+      highlight: "#FFC870",
+      label: "Yellow"
+    }, {
+      value: 40,
+      color: "#949FB1",
+      highlight: "#A8B3C5",
+      label: "Grey"
+    }, {
+      value: 120,
+      color: "#4D5360",
+      highlight: "#616774",
+      label: "Dark Grey"
+    }
+
+  ]
+});
--- dist/assets/ember-cli-chartjs.js
+++ dist/assets/ember-cli-chartjs.js
@@ -117,6 +117,127 @@ define('ember-cli-chartjs/components/ember-chart', ['exports', 'ember'], functio
   });
 
 });
+define('ember-cli-chartjs/controllers/index', ['exports', 'ember'], function (exports, Ember) {
+
+  'use strict';
+
+  exports['default'] = Ember['default'].Controller.extend({
+    pieValue1: 300,
+    pieValue2: 50,
+    pieValue3: 100,
+    pieData: (function () {
+      return [{
+        value: parseInt(this.get("pieValue1")),
+        color: "#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+      }, {
+        value: parseInt(this.get("pieValue2")),
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+      }, {
+        value: parseInt(this.get("pieValue3")),
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+      }];
+    }).property("pieValue1", "pieValue2", "pieValue3"),
+    lineValue1: 65,
+    lineValue2: 59,
+    lineData: (function () {
+      return {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+          label: "My First dataset",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [parseInt(this.get("lineValue1")), parseInt(this.get("lineValue2")), 80, 81, 56, 55, 40]
+        }, {
+          label: "My Second dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }]
+      };
+    }).property("lineValue1", "lineValue2"),
+    barData: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [{
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.5)",
+        strokeColor: "rgba(220,220,220,0.8)",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }, {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.5)",
+        strokeColor: "rgba(151,187,205,0.8)",
+        highlightFill: "rgba(151,187,205,0.75)",
+        highlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }]
+    },
+    radarData: {
+      labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+      datasets: [{
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 90, 81, 56, 55, 40]
+      }, {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 96, 27, 100]
+      }]
+    },
+    polarAreaData: [{
+      value: 300,
+      color: "#F7464A",
+      highlight: "#FF5A5E",
+      label: "Red"
+    }, {
+      value: 50,
+      color: "#46BFBD",
+      highlight: "#5AD3D1",
+      label: "Green"
+    }, {
+      value: 100,
+      color: "#FDB45C",
+      highlight: "#FFC870",
+      label: "Yellow"
+    }, {
+      value: 40,
+      color: "#949FB1",
+      highlight: "#A8B3C5",
+      label: "Grey"
+    }, {
+      value: 120,
+      color: "#4D5360",
+      highlight: "#616774",
+      label: "Dark Grey"
+    }]
+  });
+
+});
 define('ember-cli-chartjs/initializers/app-version', ['exports', 'ember-cli-chartjs/config/environment', 'ember'], function (exports, config, Ember) {
 
   'use strict';
@@ -290,6 +411,16 @@ define('ember-cli-chartjs/tests/components/ember-chart.jshint', function () {
   });
 
 });
+define('ember-cli-chartjs/tests/controllers/index.jshint', function () {
+
+  'use strict';
+
+  module('JSHint - controllers');
+  test('controllers/index.js should pass jshint', function() { 
+    ok(true, 'controllers/index.js should pass jshint.'); 
+  });
+
+});
 define('ember-cli-chartjs/tests/helpers/resolver', ['exports', 'ember/resolver', 'ember-cli-chartjs/config/environment'], function (exports, Resolver, config) {
 
   'use strict';
@@ -402,7 +533,7 @@ catch(err) {
 if (runningTests) {
   require("ember-cli-chartjs/tests/test-helper");
 } else {
-  require("ember-cli-chartjs/app")["default"].create({"name":"ember-cli-chartjs","version":"0.0.0.ae993723"});
+  require("ember-cli-chartjs/app")["default"].create({"name":"ember-cli-chartjs","version":"0.0.0.fb371602"});
 }
 
 /* jshint ignore:end */
